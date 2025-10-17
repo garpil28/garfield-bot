2de2e1b863772a9c8366ef9a1fffab0d71b37e86
garpil28 <storegarfield076@gmail.com>
2025-10-17 21:53:06 +0700

    Refactor config.py to include bot and database config

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+BOT_TOKEN = "8323328461:AAG_Khql4RCTJzkYBFo6z8fD-MvgEz4h2oQ"
+ADMIN_ID = 6840875631
+
+# Koneksi database MySQL
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "",
+    "database": "autopay_bot"
+}
